4da9cdad6e26bbd1b29fd6f26b035a01320bddf3
Anton Durov <antondurov05@protonmail.com>
2026-06-24 15:37:31 +0300

    Fixed projects.test

--- src/pages/Projects.test.tsx
+++ src/pages/Projects.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react'
 // import userEvent from '@testing-library/user-event'
 import Projects from './Projects'
-import projects from "../data/projects.json";
+import { projects } from "../data/projects.ts";
 
 describe('Projects', () => {
     it('Projects renders one <li> per entry in projects.json', () => {
