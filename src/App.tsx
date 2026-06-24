8a825ec6f82872e587e1028dab7c97aecf44b09e
Anton Durov <antondurov05@protonmail.com>
2026-06-24 15:44:48 +0300

    Moved Router to main.tsx form App component

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,14 @@ function App() {
 
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/cv" element={<CV />} />
-        </Routes>
-      </Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/cv" element={<CV />} />
+      </Routes>
     </>
   )
 }
