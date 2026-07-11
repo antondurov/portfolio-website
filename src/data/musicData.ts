e437e7fe29da02770cb01fa5c9601eaa1d9aa163
Anton Durov <antondurov05@protonmail.com>
2026-07-11 11:18:46 +0300

    Editing musicData.tsx

--- src/data/musicData.ts
+++ src/data/musicData.ts
@@ -14,18 +14,13 @@ export const musicData = {
     toja: {
       name: "Toja",
       topSongs: [
-        { title: "New Song 1", soundcloudUrl: "https://soundcloud.com/new-song-1" },
-        { title: "New Song 2", soundcloudUrl: "https://soundcloud.com/new-song-2" },
-        { title: "New Song 3", soundcloudUrl: "https://soundcloud.com/new-song-3" },
       ],
       favorites: [
-        { title: "avtomat", soundcloudUrl: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A1882155519&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true" },
-        { title: "Running Out (REMIX)", soundcloudUrl: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A1491625057&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=truehttps://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A1491625057&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"}
       ]
     },
     about: {
-      inspirations: "My inspirations come from nature, emotions, and the stories around me.",
-      futureGoals: "I aim to create music that resonates with people on a deeper level.",
-      philosophy: "Music is a universal language that connects souls and transcends boundaries.",
+      inspirations: "My inspirations come from listening to labels like NCS, Monstercat, and artists like Martin Garrix, Mesto & Brooks.",
+      futureGoals: "I asprire to tell a story through my music, to create a unique sound that resonates with listeners and leaves a lasting impact.",
+      philosophy: "Music is a universal language that can connect people from all walks of life. I believe in the power of music to evoke emotions, inspire change, and bring people together.",
     },
   };
