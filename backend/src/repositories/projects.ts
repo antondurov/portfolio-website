8b81a1827ee23e00469cc388226708b58e9e1b08
Anton Durov <antondurov05@protonmail.com>
2026-07-14 11:17:42 +0300

    Created projects.ts repository

---
+++ backend/src/repositories/projects.ts
@@ -0,0 +1,13 @@
+import { db } from "../db/db";
+import { projects } from "../db/schema";
+import { eq } from "drizzle-orm";
+
+export async function getAllProjects() {
+    return await db.select().from(projects);
+}
+
+export async function getProjectById(id: number) {
+    const result = await db.select().from(projects).where(eq(projects.id, id));
+
+    return result[0] ?? null;
+}
