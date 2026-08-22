362430ebdf16d67adaa1b24727dccf85fe6dfb97
Anton Durov <antondurov05@protonmail.com>
2026-08-22 15:27:54 +0300

    Fixed trigger inside Neon DB

--- backend/drizzle/0002_solid_morg.sql
+++ backend/drizzle/0002_solid_morg.sql
@@ -1,16 +1,3 @@
 DROP TABLE "messages" CASCADE;--> statement-breakpoint
 ALTER TABLE "projects" ADD COLUMN "created_at" timestamp with time zone DEFAULT now() NOT NULL;--> statement-breakpoint
 ALTER TABLE "projects" ADD COLUMN "updated_at" timestamp with time zone DEFAULT now() NOT NULL;
-
-CREATE OR REPLACE FUNCTION update_projects_updated_at()
-RETURNS TRIGGER AS $$
-BEGIN
-    NEW.updated_at = NOW();
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER projects_updated_at_trigger
-BEFORE UPDATE ON projects
-FOR EACH ROW
-EXECUTE FUNCTION update_projects_updated_at();
