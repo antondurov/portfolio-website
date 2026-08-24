0bd0889039d1e89db91aad0466233f66dc566679
Anton Durov <antondurov05@protonmail.com>
2026-08-24 22:46:53 +0300

    enforce return type on projets functions

--- src/backend/src/repositories/projects.ts
+++ src/backend/src/repositories/projects.ts
@@ -2,11 +2,13 @@ import { db } from "../db/db.ts";
 import { projects } from "../db/schema.ts";
 import { eq } from "drizzle-orm";
 
-export async function getAllProjects() {
+type Project = typeof projects.$inferInsert;
+
+export async function getAllProjects(): Promise<Project[]> {
     return await db.select().from(projects);
 }
 
-export async function getProjectById(id: number) {
+export async function getProjectById(id: number): Promise<Project | null> {
     const result = await db.select().from(projects).where(eq(projects.id, id));
 
     return result[0] ?? null;
