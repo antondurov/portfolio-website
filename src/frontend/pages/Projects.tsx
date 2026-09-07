5d566dbe4466fd6bc7c59522abad65a3ca9563a1
Anton Durov <antondurov05@protonmail.com>
2026-09-07 22:06:09 +0300

    Fixed import of project data inside projects.tsx

--- src/frontend/pages/Projects.tsx
+++ src/frontend/pages/Projects.tsx
@@ -1,4 +1,4 @@
-import type { Project } from "@/data/projects.ts";
+import type { Project } from "../../backend/src/data/projects.ts";
 import { useTranslation } from "react-i18next";
 
 interface ProjectsProps {
